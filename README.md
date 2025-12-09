303d3dc84851c352e2ce252a1c1db4945483002d
Martin Urban <martin.urban@studmail.w-hs.de>
2025-12-09 11:45:27 +0100

    Update README to include information about how to build the Conan RDKit package and how to build in CLion

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # rdkit-cpp-example-template
+[![License](https://img.shields.io/badge/license-BSD-blue.svg)](https://github.com/rdkit/rdkit/blob/master/LICENSE)
+[![Conan](https://img.shields.io/badge/conan-ready-brightgreen.svg)](https://conan.io/)
+
 This repository is based on the blog post [Using the RDKit in a C++ program](https://greglandrum.github.io/rdkit-blog/posts/2021-07-24-setting-up-a-cxx-dev-env.html)
 as a ready-to-clone GitHub template.
 
@@ -10,6 +13,8 @@ To get the `Conda` version, please check the `conda` branch.
 * [Build](#Build)
     * [From source](#From-source)
     * [Package as standalone folder](#Package-as-standalone-folder)
+    * [Building the Conan RDKit package](#Building-the-Conan-RDKit-package)
+* [Working with CLion](#Working-with-CLion)
 * [Using Conda](#Using-Conda)
 
 ## Prerequisite
@@ -33,7 +38,10 @@ To get the `Conda` version, please check the `conda` branch.
     ```shell
     pip install conan
     ```
-4. Run Conan configuration step
+4. Run Conan configuration step<br>
+   **IMPORTANT**: You must have a Conan package of RDKit in your local Conan cache!<br>
+   You can get the Conan RDKit package by following the guide [Building the RDKit Conan package](#Building-the-RDKit-Conan-package).<br>
+   Once you've built the Conan RDKit package, adjust `line 45` of the `conanfile.py` so that it contains the correct channel. Most common is `rdkit/2025.09.3`.
     ```shell
     conan install .
     ```
@@ -50,14 +58,55 @@ To get the `Conda` version, please check the `conda` branch.
     conan build . --profile .\profiles\macos_release
     ```
 
-**IMPORTANT**: The profiles for Linux and macOS are not yet tested. 
-
+**IMPORTANT**: Be aware that the profiles for Linux and macOS are only tested via the GitHub action workflow. 
 
 ### Package as standalone folder
 **NOTE**: This procedure has been only tested on Windows yet. For macOS and Linux proceed with caution.
 
 The default profiles located under the `profiles` folder can be used to package the library as a standalone folder.
 To change the output folder, modify the `local_package_dir` property in the `conanfile.py` file.
+
+### Building the Conan RDKit package
+In order to build this example using Conan, you'll need to build the RDKit Conan package first, so that it's 
+available via the local Conan cache.
+To do this, clone the RDKit conan package repo:
+```shell
+git clone https://github.com/urban233/rdkit-conan-package.git
+cd rdkit-conan-package
+```
+After that, proceed with creating the package:
+```bash
+conan create . rdkit/2025.09.03
+```
+
+This will build the RDKit library and create a local Conan package.
+
+---
+
+Now you can proceed with step 4 of the [From source](#From-source) section.
+
+## Working with CLion
+To build this example in IDE's such as CLion, you can use **CMake** profiles.
+Start with cloning the repository:
+```shell
+git clone https://github.com/urban233/rdkit-cpp-example-template.git
+cd rdkit-cpp-example-template
+```
+After that, install the Conan dependencies by running:
+```shell
+conan install .
+```
+Be aware that for this, the Conan RDKit package must be present in the local Conan cache.
+Then, create the **CMake** profile by running:
+```shell
+cmake . --preset=conan-default
+```
+After that you could also build the Conan RDKit package with the command:
+```shell
+cmake --build --preset=conan-release
+```
+or activate the new **CMake** profile within CLion and then use the GUI to build 
+the project.
 
 ## Using Conda
 This repository also contains a Conda variant that can be used for dependency management.
--- conanfile.py
+++ conanfile.py
@@ -1,3 +1,9 @@
+# ----------------------------------------------------------------------------#
+# This file contains source code for the rdkit-cpp-example-template repository
+# copyright (c) 2025 by Martin Urban.
+# It is unlawful to modify or remove this copyright notice.
+# Please see the accompanying LICENSE file for further information.
+# ----------------------------------------------------------------------------#
 import os
 
 from conan import ConanFile
@@ -199,126 +205,3 @@ class RDKitCppExampleConan(ConanFile):
         "PATH",
         os.path.join(self.package_folder, "bin")
       )
-
-# from conan import ConanFile
-# from conan.tools.cmake import CMake, cmake_layout, CMakeToolchain
-# from conan.tools.files import copy
-# import os
-#
-# class SmilesProcessingConan(ConanFile):
-#   name = "smiles_processing"
-#   version = "0.1.0"
-#   license = "MIT"
-#   author = "Your Name <you@example.com>"
-#   url = "https://github.com/yourusername/smiles_processing"
-#   description = "C++ library for SMILES processing using RDKit"
-#   topics = ("chemistry", "smiles", "rdkit", "boost")
-#   settings = "os", "compiler", "build_type", "arch"
-#   options = {
-#     "shared": [True, False],
-#     "build_and_package": [True, False]
-#   }
-#   default_options = {
-#     "shared": True,
-#     "build_and_package": False
-#   }
-#   exports_sources = "src/*", "CMakeLists.txt", "cmake/*"
-#   generators = "CMakeDeps", "CMakeToolchain"
-#   requires = [
-#     "rdkit/2025.09.3@urban233/conan_testing",
-#     "boost/1.89.0"
-#   ]
-#
-#   # Map dependency name -> whitelist of DLLs to include
-#   # None or empty list means include all DLLs
-#   dep_shared_whitelist = {
-#     "rdkit": None,  # copy all RDKit DLLs
-#     "boost": ["boost_iostreams", "boost_serialization"]  # only these Boost libs
-#   }
-#
-#   def layout(self):
-#     cmake_layout(self)
-#
-#   def configure(self):
-#     # Always build as shared library
-#     self.options.shared = True
-#
-#   def generate(self):
-#     tc = CMakeToolchain(self)
-#     tc.variables["CMAKE_INSTALL_PREFIX"] = os.path.join(self.source_folder, "bin")
-#     if self.settings.os in ["Linux", "Macos"]:
-#       tc.variables["CMAKE_SKIP_RPATH"] = "FALSE"
-#       tc.variables["CMAKE_BUILD_WITH_INSTALL_RPATH"] = "TRUE"
-#
-#   def build(self):
-#     cmake = CMake(self)
-#     cmake.configure()
-#     cmake.build()
-#     if self.options.build_and_package:
-#       cmake.install()
-#       self._copy_shared_libraries(os.path.join(self.source_folder, "bin"))
-#
-#   def _copy_shared_libraries(self, bin_folder):
-#     if self.settings.os == "Windows":
-#       patterns = ["*.dll"]
-#     elif self.settings.os == "Macos":
-#       patterns = ["*.dylib"]
-#     else:  # Linux
-#       patterns = ["*.so", "*.so.*"]
-#
-#     for dep_ref, dep in self.dependencies.items():
-#       dep_name = str(dep_ref).split("/")[0].lower()
-#       whitelist = self.dep_shared_whitelist.get(dep_name, None)
-#
-#       for folder in dep.cpp_info.bindirs + dep.cpp_info.libdirs:
-#         if whitelist is None:
-#           for pattern in patterns:
-#             copy(self, pattern, src=folder, dst=bin_folder, keep_path=False)
-#         else:
-#           for lib_name in whitelist:
-#             for pattern in patterns:
-#               # Better pattern matching
-#               copy(self, f"*{lib_name}*", src=folder, dst=bin_folder, keep_path=False)
-#
-#     # shared_exts = ["*.dll", "*.so", "*.so.*", "*.dylib"]
-#     # # Copy dependency shared libraries according to whitelist
-#     # for dep_ref, dep in self.dependencies.items():
-#     #   dep_name = str(dep_ref).split("/")[0].lower()  # extract package name
-#     #   whitelist = self.dep_shared_whitelist.get(dep_name, None)
-#     #
-#     #   for folder in dep.cpp_info.bindirs + dep.cpp_info.libdirs:
-#     #     if whitelist is None:  # copy everything
-#     #       for ext in shared_exts:
-#     #         print(f"Copying {ext} from {folder} to {bin_folder}")
-#     #         copy(self, ext, src=folder, dst=bin_folder, keep_path=False)
-#     #     else:
-#     #       # copy only DLLs that match the whitelist
-#     #       for lib_name in whitelist:
-#     #         for ext in shared_exts:
-#     #           pattern = f"*{lib_name}*{ext.replace('*','')}"
-#     #           print(f"Copying {pattern} from {folder} to {bin_folder}")
-#     #           copy(self, pattern, src=folder, dst=bin_folder, keep_path=False)
-#
-#   def package(self):
-#     cmake = CMake(self)
-#     cmake.install()
-#     # # Copy headers
-#     # copy(self, "*.h",
-#     #      src=os.path.join(self.source_folder, "src"),
-#     #      dst=os.path.join(self.package_folder, "include"),
-#     #      keep_path=True)
-#     #
-#     # # Copy import libraries (.lib)
-#     # copy(self, "*.lib",
-#     #      src=os.path.join(self.build_folder, "lib"),
-#     #      dst=os.path.join(self.package_folder, "lib"),
-#     #      keep_path=False)
-#
-#     # Prepare bin folder
-#     self._copy_shared_libraries(os.path.join(self.package_folder, "bin"))
-#
-#   def package_info(self):
-#     self.cpp_info.libs = ["smiles_processing"]
-#     self.cpp_info.includedirs = ["include"]
-#     self.cpp_info.libdirs = ["lib"]
-#     self.cpp_info.bindirs = ["bin"]
